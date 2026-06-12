5d7795ee87a282f5eaa34598c84e0f56514cac46
harshitha-tr18 <1nc24ci037@ncetmail.com>
2026-06-12 09:32:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-def add(a,b):
-  return a+b
-if __name="__main__":
-  result =  add(2,3)
-  print(result)
+def add(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    result = add(2, 3)
+    print(result)
